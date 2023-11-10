bfea4f9955e0876e570784972e05d75096436bb0
Michael Enion <caewok_2000@yahoo.com>
2023-11-10 09:51:58 -0800

    More testing

---
+++ testing/wall_test_placeable.js
@@ -0,0 +1,346 @@
+Draw = CONFIG.GeometryLib.Draw;
+Point3d = CONFIG.GeometryLib.threeD.Point3d;
+Matrix = CONFIG.GeometryLib.Matrix;
+
+class WallGeometry extends PIXI.Geometry {
+  constructor() {
+    super();
+    this.addVertices();
+    this.addColors(); // For debugging.
+    this.addIndices();
+  }
+
+  /**
+   * Add 3d vertices.
+   * Facing +z direction
+   */
+//   addVertices() {
+//     const aVertices = [
+//       // Top, looking down
+//       -0.50,  0.50, 0.0,  // TL
+//        0.50,  0.50, 0.0,  // TR
+//        0.50, -0.50, 0.0,  // BR
+//       -0.50, -0.50, 0.0,  // BL
+//     ];
+//
+//     this.addAttribute("aVertex", aVertices, 3);
+//   }
+
+  addVertices() {
+    const aVertices = [
+      // Top, looking down
+      -0.50,  0.50, 0.0,  // TL
+       0.50,  0.50, 0.0,  // TR
+       0.50, -0.50, 0.0,  // BR
+      -0.50, -0.50, 0.0,  // BL
+    ];
+
+    this.addAttribute("aVertex", aVertices, 3);
+  }
+
+  addColors() {
+    // Color each vertex.
+    // Ignore alpha; let the shader set it.
+    const aColors = [
+      // Top: Shades of orange
+      1.0, 0.00, 0.0,
+      1.0, 0.25, 0.0,
+      1.0, 0.75, 0.0,
+      1.0, 1.00, 0.0,
+    ];
+    this.addAttribute("aColor", aColors, 3);
+  }
+
+  /**
+   * Indices to draw two triangles per face.
+   * Top, bottom, sides 0 through 3.
+   */
+  addIndices() {
+    /*
+     TL: 0
+     TR: 1
+     BR: 2
+     BL: 3
+
+      TL --- TR
+      |      |
+      |      |
+      BL --- BR
+    */
+    const indices = [
+      // Top
+      0, 1, 2, // TL - TR - BR
+      0, 2, 3, // TL - BR - BL
+
+      // Bottom
+      0, 3, 2, // TL - BL - BR
+      0, 2, 1, // TL - BR - TR
+    ];
+    this.addIndex(indices);
+  }
+}
+
+class UnitPlaceableShader extends AbstractEVShader {
+  /**
+   * Vertex shader constructs a quad and calculates the canvas coordinate and texture coordinate varyings.
+   * @type {string}
+   */
+  static vertexShader =
+  // eslint-disable-next-line indent
+`#version 300 es
+precision ${PIXI.settings.PRECISION_VERTEX} float;
+
+in vec3 aVertex;
+in vec3 aColor;
+
+out vec4 vColor;
+
+uniform mat3 translationMatrix;
+uniform mat3 projectionMatrix;
+
+uniform mat4 uPerspectiveMatrix;
+uniform mat4 uModelWorldMatrix;
+uniform mat4 uCameraMatrix;
+uniform vec3 uOffset;
+
+void main() {
+  vColor = vec4(aColor, 1.0);
+
+  vec4 modelPosition = vec4(aVertex, 1.0);
+  vec4 worldPosition = uModelWorldMatrix * modelPosition;
+
+  // vec4 worldPosition = vec4(aVertex, 1.0);
+  // vec4 worldPosition = trMat * vec4(aVertex, 1.0);
+
+  vec4 cameraPosition = uCameraMatrix * worldPosition; // For now
+  gl_Position = uPerspectiveMatrix * cameraPosition;
+
+  // gl_Position = worldPosition;
+}`;
+
+  static fragmentShader =
+  // eslint-disable-next-line indent
+`#version 300 es
+precision ${PIXI.settings.PRECISION_FRAGMENT} float;
+precision ${PIXI.settings.PRECISION_FRAGMENT} usampler2D;
+
+in vec4 vColor;
+out vec4 fragColor;
+
+void main() {
+  fragColor = vColor;
+}`;
+
+  /** @type {number} Degrees */
+  #fieldOfView = 90.0;
+
+  /** @type {number} */
+  #aspectRatio = 1.0;
+
+  /** @type {number} */
+  #zNear = 1.0;
+
+  /** @type {number} */
+  #zFar = 3.0;
+
+  /** @type {Point3d} */
+  #translation = new Point3d();
+
+  /** @type {Point3d} */
+  #rotation = new Point3d();
+
+  /** @type {Point3d} */
+  #scale = new Point3d(1, 1, 1);
+
+  // Store each piece of the world matrix for varying terms. All start as identity.
+  /** @type {Matrix} */
+  #rotationMatrix = Matrix.rotationXYZ(0, 0, 0);
+
+  /** @type {Matrix} */
+  #scaleMatrix = Matrix.scale(1, 1, 1);
+
+  /** @type {Matrix} */
+  #translationMatrix = Matrix.translation(0, 0, 0);
+
+  /**
+   * Uniforms added to the shader.
+   * TODO: Replace offset with translation matrix.
+   * {number[3]} uOffset    Offset the shape in the x, y, and z directions.
+   * {number[16]} uPerspectiveMatrix    Matrix to set the perspective.
+   */
+  static defaultUniforms = {
+    uPerspectiveMatrix: Matrix.identity(4, 4).toGLSLArray(),
+    uModelWorldMatrix: Matrix.identity(4, 4).toGLSLArray(),
+    uCameraMatrix: Matrix.identity(4, 4).toGLSLArray(),
+    uOffset: [0, 0, 0]
+  };
+
+  /** @type {Point3d} */
+  #viewerPosition = new Point3d();
+
+  /** @type {Point3d} */
+  #targetPosition = new Point3d();
+
+  static create(viewerPosition, targetPosition, defaultUniforms = {}) {
+    const res = super.create(defaultUniforms);
+    res.viewerPosition = viewerPosition;
+    res.targetPosition = targetPosition;
+
+    res.calculatePerspectiveMatrix();
+    res.calculateModelWorldMatrix();
+    return res;
+  }
+
+  set viewerPosition(value) {
+    this.#targetPosition.copyPartial(value);
+    this.calculateCameraMatrix();
+  }
+
+  set targetPosition(value) {
+    this.#targetPosition.copyPartial(value);
+    this.calculateCameraMatrix();
+  }
+
+  set offset(value) {
+    if ( Object.hasOwn(value, "x") ) this.uniforms.uOffset[0] = value.x;
+    if ( Object.hasOwn(value, "y") ) this.uniforms.uOffset[1] = value.y;
+    if ( Object.hasOwn(value, "z") ) this.uniforms.uOffset[2] = value.z;
+  }
+
+  get scale() { return this.#scale; }
+
+  get scaleMatrix() { return this.#scaleMatrix; }
+
+  set scale(scalePoint) {
+    this.#scale.copyFrom(scalePoint);
+    this.#scaleMatrix = Matrix.scale(this.#scale.x, this.#scale.y, this.#scale.z);
+    this.calculateModelWorldMatrix();
+  }
+
+  get rotation() { return this.#rotation; }
+
+  get rotationMatrix() { return this.#rotationMatrix; }
+
+  set rotation(rotationPoint) {
+    this.#rotation.copyFrom(rotationPoint);
+    this.#rotationMatrix = Matrix.rotationXYZ(this.#rotation.x, this.#rotation.y, this.#rotation.z);
+    this.calculateModelWorldMatrix();
+  }
+
+  get translation() { return this.#translation; }
+
+  get translationMatrix() { return this.#translationMatrix; }
+
+  set translation(translationPoint) {
+    this.#translation.copyFrom(translationPoint);
+    this.#translationMatrix = Matrix.translation(this.#translation.x, this.#translation.y, this.#translation.z);
+    this.calculateModelWorldMatrix();
+  }
+
+  set fieldOfView(value) {
+    value = Math.normalizeDegrees(value);
+    this.#fieldOfView = value;
+    this.calculatePerspectiveMatrix();
+  }
+
+  set aspectRatio(value) {
+    if ( value <= 0 ) {
+      console.error("Aspect ratio must be greater than 0.");
+      return;
+    }
+    this.#aspectRatio = value;
+    this.calculatePerspectiveMatrix();
+  }
+  set zNear(value) {
+    if ( value <= 0 ) {
+      console.error("zNear must be greater than 0.");
+      return;
+    }
+    this.#zNear = value;
+    this.calculatePerspectiveMatrix();
+  }
+
+  set zFar(value) {
+    if ( value <= 0 ) {
+      console.error("zFar must be greater than 0.");
+      return;
+    }
+    this.#zFar = value;
+    this.calculatePerspectiveMatrix();
+  }
+
+  get perspectiveMatrix() { return this.uniforms.uPerspectiveMatrix; }
+
+  get worldMatrix() { return this.uniforms.uModelWorldMatrix; }
+
+  calculatePerspectiveMatrix() {
+    const fovy = this.#fieldOfView;
+    const aspect = this.#aspectRatio;
+    const zNear = this.#zNear;
+    const zFar = this.#zFar;
+    this.uniforms.uPerspectiveMatrix = Matrix.perspectiveDegrees(fovy, aspect, zNear, zFar).toGLSLArray();
+  }
+
+  calculateModelWorldMatrix() {
+    const fullMat = Matrix.empty(4, 4);
+    // Do rotation first, assuming the models are centered at 0,0,0.
+    // Then scale, and finally translate.
+    this.#rotationMatrix.multiply4x4(this.#scaleMatrix, fullMat);
+    fullMat.multiply4x4(this.#translationMatrix, fullMat);
+    this.uniforms.uModelWorldMatrix = fullMat.toGLSLArray();
+  }
+
+  calculateCameraMatrix() {
+    const res = Matrix.lookAt(this.#viewerPosition, this.#targetPosition);
+    this.lookAtM = res; // For debugging
+    this.uniforms.uCameraMatrix = res.Minv.toGLSLArray();
+  }
+}
+
+// See https://austinmorlan.com/posts/opengl_matrices/
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Rotate around an axis
+async function rotate(axis = "x") {
+  for ( let i = 0; i < 360; i += 1 ) {
+    const change = Math.toRadians(1);
+    const rot = shader.rotation;
+    const changePt = new Point3d(rot.x, rot.y, rot.z);
+    changePt[axis] += change;
+    shader.rotation = changePt;
+    await sleep(50)
+  }
+}
+
+viewer = _token
+let [target] = game.user.targets;
+
+viewerPt = Point3d.fromTokenCenter(viewer);
+targetPt = Point3d.fromTokenCenter(target);
+
+
+geom = new WallGeometry();
+shader = UnitPlaceableShader.create(viewerPt, targetPt);
+mesh = new PIXI.Mesh(geom, shader);
+canvas.stage.addChild(mesh);
+
+// Identity matrix for world and camera for testing
+shader.uniforms.uCameraMatrix = Matrix.identity(4, 4).toGLSLArray();
+shader.uniforms.uModelWorldMatrix = Matrix.identity(4, 4).toGLSLArray();
+
+
+// Activate culling to not draw opposite faces.
+mesh.state.culling = true
+mesh.state.clockwiseFrontFace = true
+
+
+
+shader.translation = new Point3d(0, 0, -2)
+shader.rotation = new Point3d(Math.toRadians(30), 0, 0);
+shader.aspectRatio = window.outerWidth / window.outerHeight
+
+await rotate("y")
+canvas.stage.removeChild(mesh);
