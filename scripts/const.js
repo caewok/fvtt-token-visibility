c2952e108a51a974fafb8b96886500eb0637e5a1
Michael Enion <caewok_2000@yahoo.com>
2023-11-29 09:07:46 -0800

    Remove Perfect Vision mods and unused methods in visibility los

--- scripts/const.js
+++ scripts/const.js
@@ -16,11 +16,7 @@ export const FLAGS = {
 
 export const MODULES_ACTIVE = {
   WALL_HEIGHT: false,
-  PERFECT_VISION: false,
   LEVELS: false,
-  DFREDS_CE: false,
-  SIMBULS_CC: false,
-  MIDI_QOL: false,
   EV: false
 };
 
@@ -38,10 +34,6 @@ export const DEBUG = {
 // Hook init b/c game.modules is not initialized at start.
 Hooks.once("init", function() {
   MODULES_ACTIVE.WALL_HEIGHT = game.modules.get("wall-height")?.active;
-  MODULES_ACTIVE.PERFECT_VISION = game.modules.get("perfect-vision")?.active;
   MODULES_ACTIVE.LEVELS = game.modules.get("levels")?.active;
-  MODULES_ACTIVE.DFREDS_CE = game.modules.get("dfreds-convenient-effects")?.active;
-  MODULES_ACTIVE.SIMBULS_CC = game.modules.get("simbuls-cover-calculator")?.active;
-  MODULES_ACTIVE.MIDI_QOL = game.modules.get("midi-qol")?.active;
   MODULES_ACTIVE.ELEVATED_VISION = game.modules.get("elevatedvision")?.active;
 });
