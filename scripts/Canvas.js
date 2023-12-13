433eed81db4c00f75ef38ac60df4bad4487b0f1f
Michael Enion <caewok_2000@yahoo.com>
2023-12-13 11:41:40 -0800

    Fix error thrown on canvas deactivation re null geometry
    Geometry was getting destroyed but the graphics object not removed fast enough from the token layer children.

---
+++ scripts/Canvas.js
@@ -0,0 +1,30 @@
+/* globals
+*/
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+"use strict";
+
+import { MODULE_ID } from "./const.js";
+
+// Patches for the Canvas class
+export const PATCHES = {};
+PATCHES.BASIC = {};
+
+// ----- NOTE: Wraps ----- //
+
+/**
+ * A hook event that fires when the Canvas is deactivated.
+ * Needed here because the destroy token hook is too late; by then, the children of the
+ * token layer are already removed and so the graphics geometry gets destroyed twice.
+ * @event canvasTearDown
+ * @category Canvas
+ * @param {Canvas} canvas   The Canvas instance being deactivated
+ */
+function canvasTearDown(canvas) {
+  canvas.tokens.placeables.forEach(token => {
+    const losCalc = token.vision?.[MODULE_ID]?.losCalc;
+    if ( !losCalc ) return;
+    losCalc.destroy();
+  });
+}
+
+PATCHES.BASIC.HOOKS = { canvasTearDown };
