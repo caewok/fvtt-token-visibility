a34e9e7b1b20e48c8e10ea0b877d949d50a4b107
Michael Enion <caewok_2000@yahoo.com>
2024-01-23 10:02:43 -0800

    Fix call to Settings.set

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -149,7 +149,7 @@ class ChangelogBuilder {
         dont_show_again: {
           icon: `<i class="fas fa-times"></i>`,
           label: "Don't show again",
-          callback: () => Settings.get(Settings.KEYS.CHANGELOG, next)
+          callback: () => Settings.set(Settings.KEYS.CHANGELOG, next)
         }
       },
       default: "dont_show_again"
