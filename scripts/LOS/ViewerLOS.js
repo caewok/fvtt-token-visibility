f586c938a8a1d6885aa7bf86aae8211a30de8aec
Michael Enion <caewok_2000@yahoo.com>
2025-11-17 17:20:47 -0800

    🎸 feat|Viewpoints|Add back mid corner and mid sides
    Split token into thirds to distinguish mid points from front and back.

--- scripts/LOS/ViewerLOS.js
+++ scripts/LOS/ViewerLOS.js
@@ -56,18 +56,20 @@ export class ViewerLOS {
     CENTER: 0,	    			// e.g., 00000001
     CORNERS: {
       FACING: 1,				  // e.g., 00000010
-      BACK: 2,
+      MID: 2,
+      BACK: 3,
     },
-    MID: {
-      FACING: 3,
-      BACK: 4,
+    SIDES: {
+      FACING: 4,
+      MID: 5,
+      BACK: 6,
     },
     D3: {
       // If none of TOP, MID, or BOTTOM, then midpoint is assumed.
       // Otherwise, MID may be omitted.
-      TOP: 5,
-      MID: 6,
-      BOTTOM: 7,
+      TOP: 7,
+      MID: 8,
+      BOTTOM: 9,
     }
   };
 
@@ -105,7 +107,7 @@ export class ViewerLOS {
   static defaultConfiguration = {
     // Viewpoint configuration
     viewpointIndex: 1, // Center point only.
-    viewpointInset: 0,
+    viewpointInset: 0, // Percentage inset
     angle: true, // If constrained by the viewer vision angle
     threshold: 0.75, // Percent used for LOS
   };
@@ -295,7 +297,7 @@ export class ViewerLOS {
    * @param {number} [inset=1]
    * @returns {boolean} If no constrained border, returns true. Otherwise true if within the constrained border.
    */
-  static testPointOutsideConstrainedBorder(pt, token, inset = 1) {
+  static testPointOutsideConstrainedBorder(pt, token, inset = 0) {
     const constrainedBorder = token.constrainedTokenBorder;
     if ( constrainedBorder.equals(token.tokenBorder) ) return false;
     if ( inset > 0 ) return true;
@@ -381,7 +383,7 @@ export class ViewerLOS {
     const midZ = topZ - bottomZ;
 
     // Set either all 4 corners or a subset.
-    const cornerMask = SmallBitSet.fromIndices([PI.CORNERS.FACING, PI.CORNERS.BACK]);
+    const cornerMask = SmallBitSet.fromIndices([PI.CORNERS.FACING, PI.CORNERS.MID, PI.CORNERS.BACK]);
     const cornerIx = bs.intersectionNew(cornerMask);
     if ( cornerIx.equals(cornerMask) ) {
       cornerPoints = this.getCorners(tokenShape, midZ);
@@ -390,11 +392,12 @@ export class ViewerLOS {
       cornerPoints = this.getCorners(tokenShape, midZ);
       const res = this._facingPoints(cornerPoints, token, viewpoint);
       if ( cornerIx.hasIndex(PI.CORNERS.FACING) ) tokenPoints.push(...res.facing);
+      if ( cornerIx.hasIndex(PI.CORNERS.MID) ) tokenPoints.push(...res.mid);
       if ( cornerIx.hasIndex(PI.CORNERS.BACK) ) tokenPoints.push(...res.back);
     }
 
     // Set either all side points or a subset
-    const sidesMask = SmallBitSet.fromIndices([PI.MID.FACING, PI.MID.BACK, PI.MID.SIDES]);
+    const sidesMask = SmallBitSet.fromIndices([PI.SIDES.FACING, PI.SIDES.MID, PI.SIDES.BACK]);
     const sidesIx = bs.intersectionNew(sidesMask);
     if ( !sidesIx.isEmpty ) {
        cornerPoints ??= this.getCorners(tokenShape, midZ);
@@ -402,8 +405,9 @@ export class ViewerLOS {
        if ( sidesIx.equals(sidesMask) ) tokenPoints.push(...mids); // Include all sides.
        else {
          const res = this._facingPoints(mids, token, viewpoint);
-         if ( sidesIx.hasIndex(PI.MID.FACING) ) tokenPoints.push(...res.facing);
-         if ( sidesIx.hasIndex(PI.MID.BACK) ) tokenPoints.push(...res.back);
+         if ( sidesIx.hasIndex(PI.SIDES.FACING) ) tokenPoints.push(...res.facing);
+         if ( sidesIx.hasIndex(PI.SIDES.MID) ) tokenPoints.push(...res.mid);
+         if ( sidesIx.hasIndex(PI.SIDES.BACK) ) tokenPoints.push(...res.back);
        }
     }
 
@@ -453,31 +457,55 @@ export class ViewerLOS {
     // Token rotation is 0º for due south, while Ray is 0º for due east.
     // Token rotation is 90º for due west, while Ray is 90º for due south.
     // Use the Ray version to divide the token into front and back.
+    // Divide the token into thirds : front third, mid third (sides), back third.
     const facing = [];
+    const mid = [];
     const back = [];
     const center = Point3d.fromTokenCenter(token);
     let b;
+    let dir = Point3d.tmp;
+    let dirPerp = Point3d.tmp;
+
     if ( viewpoint ) {
       // Determine the line perpendicular to the center --> viewpoint line and use to sort the points.
-      const dir = viewpoint.subtract(center);
-      const dirPerp = Point3d.tmp.set(dir.y, -dir.x, 0); // (-dir.y, dir.x) flips front/back.
-      b = center.add(dirPerp);
-      dir.release();
-      dirPerp.release();
+      viewpoint.subtract(center, dir);
+      dirPerp.set(dir.y, -dir.x, 0); // (-dir.y, dir.x) flips front/back.
+
     } else {
       // Token rotation is 0º for due south, while Ray is 0º for due east.
       // Token rotation is 90º for due west, while Ray is 90º for due south.
       // Use the Ray version to divide the token into front and back.
       const angle = Math.toRadians(token.document.rotation);
       b = PIXI.Point.fromAngle(center, angle, 100);
+      const dirPerp2d = b.subtract(center);
+      dirPerp.set(dirPerp2d.x, dirPerp2d.y, 0);
+      dir.set(dirPerp.y, -dirPerp.x, 0);
+      dirPerp2d.release();
     }
+
+    const dist2d = Math.min(token.w, token.h) * 0.33;
+    dir.normalize(dir).multiplyScalar(dist2d, dir);
+    dirPerp.normalize(dirPerp);
+    const aFront = center.add(dir);
+    const aBack = center.subtract(dir);
+    const bFront = aFront.add(dirPerp);
+    const bBack = aBack.add(dirPerp);
+
+    const orient2d = foundry.utils.orient2dFast;
+    const oCenter = orient2d(aFront, bFront, center);
     pts.forEach(pt => {
-      const arr = foundry.utils.orient2dFast(center, b, pt) > 0 ? back : facing;
+      const arr = orient2d(aFront, bFront, pt) * oCenter >= 0 ? facing
+        : orient2d(aBack, bBack, pt) * oCenter >= 0 ? back : mid;
       arr.push(pt);
     });
     center.release();
-    b.release();
-    return { facing, back };
+    dir.release();
+    dirPerp.release();
+    aFront.release();
+    aBack.release();
+    bFront.release();
+    bBack.release();
+    return { facing, mid, back };
   }
 
   /**
